5d3faba21eecbf005be042398a44f882365022d5
Galen Weld <gweld@cs.washington.edu>
2018-12-03 17:51:44 -0800

    predictions now working

--- sliding_window/sliding_window.py
+++ sliding_window/sliding_window.py
@@ -1,8 +1,14 @@
 import base64, sys, json
 import tensorflow as tf
 import os
+from oauth2client.client import GoogleCredentials
+from googleapiclient import discovery
 
+# will need to install Google API client
+# pip install --upgrade google-api-python-client
 
+GCLOUD_SERVICE_KEY = '/home/gweld/gcloud_acct_key.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GCLOUD_SERVICE_KEY
 
 PROJECT = 'sidewalk-dl'
 BUCKET = 'sidewalk_crops_subset'
@@ -12,24 +18,9 @@ MODEL = 'sidewalk'
 VERSION = 'resnet'
 
 def predict_json(project, model, instances, version=None):
-    """Send json data to a deployed model for prediction.
-
-    Args:
-        project (str): project where the Cloud ML Engine Model is deployed.
-        model (str): model name.
-        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
-            your deployed model expects as inputs. Values should be datatypes
-            convertible to Tensors, or (potentially nested) lists of datatypes
-            convertible to tensors.
-        version: str, version of the model to target.
-    Returns:
-        Mapping[str: any]: dictionary of prediction results defined by the
-            model.
-    """
-    # Create the ML Engine service object.
-    # To authenticate set the environment variable
-    # GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
-    service = googleapiclient.discovery.build('ml', 'v1')
+    # from:
+    # https://cloud.google.com/ml-engine/docs/tensorflow/online-predict#creating_models_and_versions
+    service = discovery.build('ml', 'v1')
     name = 'projects/{}/models/{}'.format(project, model)
 
     if version is not None:
@@ -47,10 +38,18 @@ def predict_json(project, model, instances, version=None):
 
 
 #with tf.gfile.FastGFile('gs://sidewalk_crops_subset/img/2.jpg', 'r') as ifp:
-with open('null14.jpg', 'r') as ifp, open('test.json', 'w') as ofp:
-    image_data = ifp.read()
-    img = base64.b64encode(image_data)
-    json.dump({"image_bytes": {"b64": img}}, ofp)
+# with open('null14.jpg', 'r') as ifp, open('test.json', 'w') as ofp:
+#     image_data = ifp.read()
+#     img = base64.b64encode(image_data)
+#     json.dump({"image_bytes": {"b64": img}}, ofp)
 
-#predictions = predict_json(PROJECT, MODEL, xx, VERSION)
-#print predictions
+def predict_label(img_path):
+	with open(img_path, 'r') as imgfile:
+		image_data = imgfile.read()
+		img = base64.b64encode(image_data)
+		instances = {'image_bytes' : {'b64': img}}
+		predictions = predict_json(PROJECT, MODEL, instances, VERSION)
+		print predictions
+
+predict_label('38.jpg')
+
