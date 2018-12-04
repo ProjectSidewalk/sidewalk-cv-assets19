ed66abf4a826c02f472ef69b1ee66eb993aec6a4
Galen Weld <gweld@cs.washington.edu>
2018-12-03 21:44:08 -0800

    first working version of sliding window, still needs better pooling but its a start

--- sliding_window/sliding_window.py
+++ sliding_window/sliding_window.py
@@ -1,10 +1,12 @@
 import base64, sys, json, os
 import tensorflow as tf
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from oauth2client.client import GoogleCredentials
 from googleapiclient import discovery
 import numpy as np
 import math
+from collections import defaultdict
+import csv
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #from GSV import GSVImage
 #from GSV.utilities import *
@@ -59,82 +61,82 @@ def predict_label(img_path):
 
 
 def bilinear_interpolation(x, y, points):
-    '''Interpolate (x,y) from values associated with four points.
+	'''Interpolate (x,y) from values associated with four points.
 
-    The four points are a list of four triplets:  (x, y, value).
-    The four points can be in any order.  They should form a rectangle.
+	The four points are a list of four triplets:  (x, y, value).
+	The four points can be in any order.  They should form a rectangle.
 
-        >>> bilinear_interpolation(12, 5.5,
-        ...                        [(10, 4, 100),
-        ...                         (20, 4, 200),
-        ...                         (10, 6, 150),
-        ...                         (20, 6, 300)])
-        165.0
-    
-    Code written by Raymond Hettinger. Check:
-    http://stackoverflow.com/questions/8661537/how-to-perform-bilinear-interpolation-in-python
-    
-    Modified by Kotaro.
-    In case four points have same x values or y values, perform linear interpolation
-    '''
-    # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
+		>>> bilinear_interpolation(12, 5.5,
+		...                        [(10, 4, 100),
+		...                         (20, 4, 200),
+		...                         (10, 6, 150),
+		...                         (20, 6, 300)])
+		165.0
+	
+	Code written by Raymond Hettinger. Check:
+	http://stackoverflow.com/questions/8661537/how-to-perform-bilinear-interpolation-in-python
+	
+	Modified by Kotaro.
+	In case four points have same x values or y values, perform linear interpolation
+	'''
+	# See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
 
-    points = sorted(points)               # order points by x, then by y
-    (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
+	points = sorted(points)               # order points by x, then by y
+	(x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
 
 
-    if (x1 == _x1) and (x1 == x2) and (x1 == _x2):
-        if x != x1:
-            raise ValueError('(x, y) not on the x-axis')
-        if y == y1:
-            return q11
-        return (q11 * (_y2 - y) + q22 * (y - y1)) / ((_y2 - y1) + 0.0)
-    if (y1 == _y1) and (y1 == y2) and (y1 == _y2):
-        if y != y1 :
-            raise ValueError('(x, y) not on the y-axis')
-        if x == x1:
-            return q11
-        return (q11 * (_x2 - x) + q22 * (x - x1)) / ((_x2 - x1) + 0.0)
-            
+	if (x1 == _x1) and (x1 == x2) and (x1 == _x2):
+		if x != x1:
+			raise ValueError('(x, y) not on the x-axis')
+		if y == y1:
+			return q11
+		return (q11 * (_y2 - y) + q22 * (y - y1)) / ((_y2 - y1) + 0.0)
+	if (y1 == _y1) and (y1 == y2) and (y1 == _y2):
+		if y != y1 :
+			raise ValueError('(x, y) not on the y-axis')
+		if x == x1:
+			return q11
+		return (q11 * (_x2 - x) + q22 * (x - x1)) / ((_x2 - x1) + 0.0)
+			
 
-    if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
-        raise ValueError('points do not form a rectangle')
-    if not x1 <= x <= x2 or not y1 <= y <= y2:
-        print "x, y, x1, x2, y1, y2", x, y, x1, x2, y1, y2 
-        raise ValueError('(x, y) not within the rectangle')
+	if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
+		raise ValueError('points do not form a rectangle')
+	if not x1 <= x <= x2 or not y1 <= y <= y2:
+		print "x, y, x1, x2, y1, y2", x, y, x1, x2, y1, y2 
+		raise ValueError('(x, y) not within the rectangle')
 
-    return (q11 * (x2 - x) * (y2 - y) +
-            q21 * (x - x1) * (y2 - y) +
-            q12 * (x2 - x) * (y - y1) +
-            q22 * (x - x1) * (y - y1)
-           ) / ((x2 - x1) * (y2 - y1) + 0.0)
+	return (q11 * (x2 - x) * (y2 - y) +
+			q21 * (x - x1) * (y2 - y) +
+			q12 * (x2 - x) * (y - y1) +
+			q22 * (x - x1) * (y - y1)
+		   ) / ((x2 - x1) * (y2 - y1) + 0.0)
 
 
 def interpolated_3d_point(xi, yi, x_3d, y_3d, z_3d, scale=26):
-    """
-     This function takes a GSV image point (xi, yi) and 3d point cloud data (x_3d, y_3d, z_3d) and 
-     returns its estimated 3d point. 
-    """
-    xi = float(xi) / scale
-    yi = float(yi) / scale
-    xi1 = int(math.floor(xi))
-    xi2 = int(math.ceil(xi))
-    yi1 = int(math.floor(yi))
-    yi2 = int(math.ceil(yi))
-    
-    if xi1 == xi2 and yi1 == yi2:
-        val_x = x_3d[yi1, xi1]
-        val_y = y_3d[yi1, xi1]
-        val_z = z_3d[yi1, xi1]
-    else:
-        points_x = ((xi1, yi1, x_3d[yi1, xi1]),   (xi1, yi2, x_3d[yi2, xi1]), (xi2, yi1, x_3d[yi1, xi2]), (xi2, yi2, x_3d[yi2, xi2]))         
-        points_y = ((xi1, yi1, y_3d[yi1, xi1]),   (xi1, yi2, y_3d[yi2, xi1]), (xi2, yi1, y_3d[yi1, xi2]), (xi2, yi2, y_3d[yi2, xi2]))
-        points_z = ((xi1, yi1, z_3d[yi1, xi1]),   (xi1, yi2, z_3d[yi2, xi1]), (xi2, yi1, z_3d[yi1, xi2]), (xi2, yi2, z_3d[yi2, xi2]))                  
-        val_x = bilinear_interpolation(xi, yi, points_x)
-        val_y = bilinear_interpolation(xi, yi, points_y)
-        val_z = bilinear_interpolation(xi, yi, points_z)
-    
-    return (val_x, val_y, val_z)
+	"""
+	 This function takes a GSV image point (xi, yi) and 3d point cloud data (x_3d, y_3d, z_3d) and 
+	 returns its estimated 3d point. 
+	"""
+	xi = float(xi) / scale
+	yi = float(yi) / scale
+	xi1 = int(math.floor(xi))
+	xi2 = int(math.ceil(xi))
+	yi1 = int(math.floor(yi))
+	yi2 = int(math.ceil(yi))
+	
+	if xi1 == xi2 and yi1 == yi2:
+		val_x = x_3d[yi1, xi1]
+		val_y = y_3d[yi1, xi1]
+		val_z = z_3d[yi1, xi1]
+	else:
+		points_x = ((xi1, yi1, x_3d[yi1, xi1]),   (xi1, yi2, x_3d[yi2, xi1]), (xi2, yi1, x_3d[yi1, xi2]), (xi2, yi2, x_3d[yi2, xi2]))         
+		points_y = ((xi1, yi1, y_3d[yi1, xi1]),   (xi1, yi2, y_3d[yi2, xi1]), (xi2, yi1, y_3d[yi1, xi2]), (xi2, yi2, y_3d[yi2, xi2]))
+		points_z = ((xi1, yi1, z_3d[yi1, xi1]),   (xi1, yi2, z_3d[yi2, xi1]), (xi2, yi1, z_3d[yi1, xi2]), (xi2, yi2, z_3d[yi2, xi2]))                  
+		val_x = bilinear_interpolation(xi, yi, points_x)
+		val_y = bilinear_interpolation(xi, yi, points_y)
+		val_z = bilinear_interpolation(xi, yi, points_z)
+	
+	return (val_x, val_y, val_z)
 
 
 def extract_panoyawdeg(path_to_metadata_xml):
@@ -150,19 +152,19 @@ def extract_panoyawdeg(path_to_metadata_xml):
 
 
 def get_depth_at_location(path_to_depth_txt, xi, yi):
-    depth_location = path_to_depth_txt
+	depth_location = path_to_depth_txt
 
-    filename = depth_location
+	filename = depth_location
 
-    with open(filename, 'rb') as f:
-        depth = np.loadtxt(f)
+	with open(filename, 'rb') as f:
+		depth = np.loadtxt(f)
 
-    depth_x = depth[:, 0::3]
-    depth_y = depth[:, 1::3]
-    depth_z = depth[:, 2::3]
+	depth_x = depth[:, 0::3]
+	depth_y = depth[:, 1::3]
+	depth_z = depth[:, 2::3]
 
-    val_x, val_y, val_z = interpolated_3d_point(xi, yi, depth_x, depth_y, depth_z)
-    return val_x, val_y, val_z
+	val_x, val_y, val_z = interpolated_3d_point(xi, yi, depth_x, depth_y, depth_z)
+	return val_x, val_y, val_z
 
 
 def predict_crop_size(x, y, im_width, im_height, path_to_depth_file):
@@ -257,17 +259,18 @@ def make_single_crop_from_depth(path_to_image, sv_image_x, sv_image_y, PanoYawDe
 	return
 
 
-def make_sliding_window_crops(pano_root, stride=100, bottom_space=1600):
+def make_sliding_window_crops(pano_root, outdir, stride=100, bottom_space=1600, side_space=500):
+	''' side padding is a multiple of stride '''
 	pano_img_path   = pano_root + ".jpg"
 	pano_xml_path   = pano_root + ".xml"
 	pano_depth_path = pano_root + ".depth.txt"
 	pano_yaw_deg = extract_panoyawdeg(pano_xml_path)
 
-	x, y = 2 * stride, 0
+	x, y = side_space, 0
 	while(y > - (gsv_image_height/2 - bottom_space)):
-		while(x < gsv_image_width - (2*stride)):
+		while(x < gsv_image_width - side_space):
 			# do things in one row
-			output_filename  = "test_crops/{},{}.jpg".format(x,y)
+			output_filename  = os.path.join(outdir, "{},{}.jpg".format(x,y))
 			print "cropping around ({},{})".format(x,y)
 			try:
 				make_single_crop_from_depth(pano_img_path, x, y, pano_yaw_deg, pano_depth_path, output_filename)
@@ -277,7 +280,119 @@ def make_sliding_window_crops(pano_root, stride=100, bottom_space=1600):
 		y -= stride
 		x = 2 * stride
 
+predictions = {'1600,-500':[.5, 0, 0, 0], "3200,-800":[.1, 0, 0, 0]}
 
 
-make_sliding_window_crops('1_1OfETDixMMCUhSWn-hcA', stride=1000)
+def predict_from_crops(crops_dir):
+	''' returns dict mapping string of coords to list of predictions '''
+	predictions = defaultdict(list)
+
+	for imagename in os.listdir(crops_dir):
+		if not imagename.endswith('.jpg'): continue
+		coords = imagename[:-4]
+		x,y = coords.split(',')
+
+		prediction = predict_label(os.path.join(crops_dir, imagename))
+
+		print "getting predictions for {}".format(imagename)
+		print prediction
+
+		predictions[coords] = prediction
+
+	return predictions
+
+
+def write_predictions_to_file(predictions, path):
+	count = 0
+	with open(path, 'w') as csvfile:
+		writer = csv.writer(csvfile)
+
+		for coods, prediction in predictions.iteritems():
+			x,y = coods.split(',')
+			row = [x,y] + prediction
+			writer.writerow(row)
+			count += 1
+		print "Wrote {} predictions to {}.".format(count, path)
+
+
+def read_predictions_from_file(path):
+	predictions = defaultdict(list)
+
+	with open(path, 'r') as csvfile:
+		reader = csv.reader(csvfile)
+
+		for row in reader:
+			x, y = row[0], row[1]
+			prediction = map(float, row[2:])
+			predictions["{},{}".format(x,y)] = prediction
+	return predictions
+
+
+def predict(prediction):
+	''' applies an algorithm to return the predicted class
+		returns None if there's no prediction '''
+
+	# currently returns the strongest prediction if either one
+	# is >.5, otherwise returns None
+
+	# Only look at ramps and missing ramps
+	ramp = prediction[0]
+	missing = prediction[1]
+	overall = max(ramp, missing)
+	best = "Curb Ramp" if ramp > missing else "Missing Ramp"
+
+	if overall < .85: return None
+	return best
+
+
+def annotate(img, pano_yaw_deg, coords, label):
+	""" takes in an image object and labels it at specified coords
+		translates streetview coords to pixel coords """
+	sv_x, sv_y = coords
+	x = ((float(pano_yaw_deg) / 360) * gsv_image_width + sv_x) % gsv_image_width
+	y = gsv_image_height / 2 - sv_y
+
+	# radius for dot
+	r = 20
+	draw = ImageDraw.Draw(img)
+	draw.ellipse((x - r, y - r, x + r, y + r), fill=128)
+
+	font  = ImageFont.truetype("roboto.ttf", 60, encoding="unic")
+	draw.text((x+r+10, y), label, fill=128, font=font)
+
+
+def show_predictions_on_image(pano_root, predictions, out_img):
+	pano_img_path   = pano_root + ".jpg"
+	pano_xml_path   = pano_root + ".xml"
+	pano_depth_path = pano_root + ".depth.txt"
+	pano_yaw_deg = extract_panoyawdeg(pano_xml_path)
+
+	img = Image.open(pano_img_path)
+
+	# Test annotation
+	#annotate(img, pano_yaw_deg, (9200,-800), "Curb Ramp")
+
+	count = 0
+	for coords, prediction in predictions.iteritems():
+		sv_x, sv_y = map(int, coords.split(','))
+		label = predict(prediction)
+		if label is not None:
+			print "Found a {} at ({},{})".format(label, sv_x, sv_y)
+			annotate(img, pano_yaw_deg, (sv_x, sv_y), label)
+			count += 1
+	img.save(out_img)
+	print "Marked {} labels on {}.".format(count, out_img)
+	return
+
+
+
+
+
+
+
+
+predictions = read_predictions_from_file('test_preds.csv')
+show_predictions_on_image('1_1OfETDixMMCUhSWn-hcA', predictions, 'preds.jpg')
+
+#make_sliding_window_crops('1_1OfETDixMMCUhSWn-hcA', test_crops, stride=100)
 
