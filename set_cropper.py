620968847602fff12c87c3690c7a8243d2d0ee92
Galen Weld <galen.weld@gmail.com>
2019-02-20 18:29:55 -0800

    moved old files out of the way

--- set_cropper.py
+++ set_cropper.py
@@ -1,4 +1,4 @@
 from GSVutils.utils import bulk_extract_crops
 
 
-bulk_extract_crops('dataset_csvs/small.csv', 'test_export/')
+bulk_extract_crops('dataset_csvs/Train.csv', 'new_ds_exports/Train/')
