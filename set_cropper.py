8d928fcf52745b79f9d7ec11ef9bbbf53a290ac4
Galen Weld <galen.weld@gmail.com>
2019-03-12 11:52:16 -0700

    started modifying sliding window to work on new models

--- set_cropper.py
+++ set_cropper.py
@@ -1,3 +1,9 @@
+# Galen Weld, Feb 2019
+# Use this to extract sliding window datasets crops and sidecars
+# from CSVs produced using make_train_test_sets.ipynb
+# row in the csv should be
+# Pano ID, SV_x, SV_y, Label, Photog Heading, Heading, Label ID 
+
 from GSVutils.utils import bulk_extract_crops
 
 
