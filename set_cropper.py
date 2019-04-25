842125ddf746f0ccd844507a9d7f24d149bff2ae
Galen Weld <galen.weld@gmail.com>
2019-04-25 14:32:36 -0700

    finished running nullcrops for newberg and seattle

--- set_cropper.py
+++ set_cropper.py
@@ -18,10 +18,12 @@ from GSVutils.utils import bulk_extract_crops
 #bulk_extract_crops('new_old_dataset_csvs/Train.csv', '/mnt/e/old_dataset/train/')
 
 # this to make newberg centered dataset
-bulk_extract_crops('new_cities/newberg-labels-researchers.csv', '/mnt/g/newberg_center_crops_researchers/', '/mnt/g/scrapes_dump_newberg')
+#bulk_extract_crops('new_cities/newberg-labels-researchers.csv', '/mnt/g/newberg_center_crops_researchers/', '/mnt/g/scrapes_dump_newberg')
 #bulk_extract_crops('new_cities/newberg-labels.csv', '/mnt/e/newberg_center_crops_all/', '/mnt/e/scrapes_dump_newberg')
+#bulk_extract_crops('new_cities/newberg-labels-null.csv', '/mnt/g/newberg_center_crops_researchers/', '/mnt/g/scrapes_dump_newberg')
 
 # use this to make seattle centered dataset
-bulk_extract_crops('new_cities/seattle-labels-researchers.csv', '/mnt/g/seattle_center_crops_researchers/', '/mnt/g/scrapes_dump_seattle')
+#bulk_extract_crops('new_cities/seattle-labels-researchers.csv', '/mnt/g/seattle_center_crops_researchers/', '/mnt/g/scrapes_dump_seattle')
 #bulk_extract_crops('new_cities/seattle-labels.csv', '/mnt/e/seattle_center_crops_all/', '/mnt/e/scrapes_dump_seattle')
+bulk_extract_crops('new_cities/seattle-labels-null.csv', '/mnt/g/seattle_center_crops_researchers/', '/mnt/g/scrapes_dump_seattle')
 
